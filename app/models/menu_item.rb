c1e447f2b59f8233292be54b41147cf6395342c6
HaYeon Kim <kimhy8376@gmail.com>
2022-09-05 15:45:54 +0100

    Add more validations

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -2,6 +2,7 @@ class MenuItem < ApplicationRecord
   belongs_to :menu
   validates :name, presence: true
   validates :category, presence: true
+  validates :description, presence: true, length: { maximum: 100 }
   validates :price, presence: true
   validates :prepare_time, presence: true
 end
