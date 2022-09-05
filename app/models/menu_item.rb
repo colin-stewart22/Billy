5336d670d9d9a748ca3c8758251f1043f664573c
HaYeon Kim <kimhy8376@gmail.com>
2022-09-05 15:35:38 +0100

    Add validations

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,7 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :price, presence: true
+  validates :prepare_time, presence: true
 end
